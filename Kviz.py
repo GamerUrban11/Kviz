b4c5ac56cf5a25cb1c96812e6af96d8b08e4c1b9
GamerUrban11 <urban.koder@gmail.com>
2021-12-28 13:32:31 +0100

    Added one question.

--- Kviz.py
+++ Kviz.py
@@ -2,14 +2,25 @@
 print ("Pozdravljen!")
 print ("Kviz se zacenja!")
 print ("Pa pojdimo na kviz Slovenije!")
+
 o=input("Katero je glavno mesto Slovenije?")
+
 if o=="Ljubljana":
     print("Odgovor je pravilen!")
 else:
     print("Odgovor je zal napacen.")
 
+
 o=input("Kateri je trenutni predsednik drzave Republike?")
+
 if o=="Borut Pahor":
+    print("Odgovor je pravilen!")
+else:
+    print("Odgovor je zal napacen.")
+
+
+o=input("Kje se je rodil France Preseren?")
+if o=="Vrba na Gorenjskem":
     print("Odgovor je pravilen!")
 else:
     print("Odgovor je zal napacen.")
